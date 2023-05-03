4ec5c743b3a28ce97e2646f962a9c8a0cb50550b
Icethunder76 <joaquin_ponte_v@hotmail.com>
2023-05-03 19:23:06 -0300

    Se realizo el punto 2B y se prepara el 3A

--- PlantsVSZombies1/src/Library.hs
+++ PlantsVSZombies1/src/Library.hs
@@ -14,25 +14,39 @@ data Zombie = UnZombie {
     dañoPorMordida:: Number
 }deriving(Show,Eq)
 
-peaShooter = UnaPlanta "" 5 0 2
-repeater = UnaPlanta "" 5 0 4
-sunflower = UnaPlanta "" 7 1 0
-nut = UnaPlanta "" 100 0 0
-tripleSunflower = UnaPlanta ""  7 3 0
-canon = UnaPlanta "" 4 0 8
+data Linea = LineaDeDefensa {
+    plantas:: [Planta],
+    zombies:: [Zombie]
+}deriving(Show,Eq)
 
-zombieBase = UnZombie "ZombieBase" [""] 1
+peaShooter = UnaPlanta "PeaShooter" 5 0 2
+repeater = UnaPlanta "Repeater" 5 0 4
+sunflower = UnaPlanta "SunFlower" 7 1 0
+nut = UnaPlanta "Nut" 100 0 0
+tripleSunflower = UnaPlanta "TripleSunflower"  7 3 0
+cherryCanon = UnaPlanta "CherryCanon" 4 0 8
+
+zombieBase = UnZombie "ZombieBase" [] 1
 ballonzombie = UnZombie "BallonZombie" ["Globo"] 1
 newspaperzombie = UnZombie "NewspaperZombie" ["Diario"] 2
 gargantuar = UnZombie "GargantuarHulkSmashPunyGod" ["PosteElectrico", "ZombieEnano"] 30
 
+linea1 = LineaDeDefensa [sunflower, sunflower, sunflower] []
+linea2 = LineaDeDefensa [peaShooter, peaShooter, sunflower, nut] [zombieBase, newspaperzombie]
+linea3 = LineaDeDefensa [sunflower, peaShooter] [gargantuar, zombieBase, zombieBase]
+
 nivelDeMuerte:: Zombie -> Number
 nivelDeMuerte = (length.nombre)
 
+cantidadAccesorios:: Zombie -> Number
+cantidadAccesorios = (length.accesorios)
+
 especialidad:: Planta -> [Char]
-especialidad    | solesQueProduce /= 0 = "Proveedora"
-                | poderDeAtaque > puntosDeVida = "Atacante"
-                | otherwise = "Defensiva"
+especialidad planta | solesQueProduce(planta) /= 0 = "Proveedora"
+                    | poderDeAtaque(planta) > puntosDeVida(planta) = "Atacante"
+                    | otherwise = "Defensiva"
 
 esPeligroso:: Zombie -> Bool
-esPeligroso zombie  | accesorios(zombie) >= 1 || nivelDeMuerte(zombie) > 10 
+esPeligroso zombie  = cantidadAccesorios(zombie) >= 1 || nivelDeMuerte(zombie) > 10
+
+-- 3 A se hace con una funcion que cree la misma linea, con la modificacion de planta o zombie
